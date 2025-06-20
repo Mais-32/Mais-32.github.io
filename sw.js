5c8baa7c15783d9140c35c43b65af58d6870f28d
Mais-32 <mais-32@hotmail.com>
2025-06-21 03:29:57 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener('install', e=>{
-  self.skipWaiting();
-});
-self.addEventListener('fetch', e=>{
-  // Network first
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
